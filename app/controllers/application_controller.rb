0746a82065e748a4639d37116a44280d06704512
Marissa Zhong <mwz2101@columbia.edu>
2018-08-04 15:49:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def cart
-    sessions[:cart.items] || []
+    session[:cart.items] || []
   end
 end
